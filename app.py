bb016610b1127779a5379699545e9f17ed43a8eb
Samuel Avike <64576736+samuelavike1@users.noreply.github.com>
2023-08-23 06:42:16 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+#importing required libraries
+
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+from sklearn import metrics 
+import warnings
+import pickle
+warnings.filterwarnings('ignore')
+from feature import FeatureExtraction
+
+#file = open("pickle/model.pkl","rb")
+#gbc = pickle.load(file)
+#file.close()
+
+with open('pickle/model.pkl', 'rb') as file:
+    gbc = pickle.load(file)
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+
+        url = request.form["url"]
+        obj = FeatureExtraction(url)
+        x = np.array(obj.getFeaturesList()).reshape(1,30) 
+
+        y_pred =gbc.predict(x)[0]
+        #1 is safe       
+        #-1 is unsafe
+        y_pro_phishing = gbc.predict_proba(x)[0,0]
+        y_pro_non_phishing = gbc.predict_proba(x)[0,1]
+        # if(y_pred ==1 ):
+        pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing*100)
+        return render_template('index.html',xx =round(y_pro_non_phishing,2),url=url )
+    return render_template("index.html", xx =-1)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
